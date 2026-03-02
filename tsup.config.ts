60e16339f3a170f3d605ecb62ae836151f12068a
Juan Cruz Fortunatti <juancfortunatti@gmail.com>
2026-03-02 10:39:32 +0100

    refactor: update exports map, add workspaces, clean up root config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,8 @@
-import type { Plugin } from "esbuild";
 import { defineConfig } from "tsup";
-
-const externalSveltePlugin: Plugin = {
-  name: "external-svelte",
-  setup(build) {
-    build.onResolve({ filter: /\.svelte$/ }, (args) => ({
-      path: args.path,
-      external: true
-    }));
-  }
-};
 
 export default defineConfig({
   entry: {
-    index: "src/index.ts",
-    "react/index": "react/index.ts",
-    "vue/index": "vue/index.ts",
-    "svelte/index": "svelte/index.ts"
+    index: "src/index.ts"
   },
   format: ["esm", "cjs"],
   dts: true,
@@ -25,7 +11,5 @@ export default defineConfig({
   clean: true,
   minify: true,
   target: "es2020",
-  tsconfig: "tsconfig.json",
-  external: ["vue", "react", "react-dom", "svelte"],
-  esbuildPlugins: [externalSveltePlugin]
+  tsconfig: "tsconfig.json"
 });
